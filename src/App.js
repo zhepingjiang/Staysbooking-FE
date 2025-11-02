cf9615cfc3482b8ad6730ec9711630f4aa812ceb
zhepingjiang <zheping.jiang@gmail.com>
2025-11-02 17:02:34 -0500

    Implement Upload Stay, and present uploaded stays in my stays section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Layout, Dropdown, Menu, Button } from "antd";
 import { UserOutlined } from "@ant-design/icons";
 import React from "react";
 import LoginPage from "./components/LoginPage";
+import HostHomePage from "./components/HostHomePage";
 
 const { Header, Content } = Layout;
 
@@ -40,6 +41,10 @@ class App extends React.Component {
   renderContent = () => {
     if (!this.state.authed) {
       return <LoginPage handleLoginSuccess={this.handleLoginSuccess} />;
+    }
+
+    if (this.state.asHost) {
+      return <HostHomePage />;
     }
 
     if (this.state.asHost) {
